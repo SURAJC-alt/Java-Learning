eef8f43d530b09264eda164e01bfb1395cdd6c12
SURAJC <csurajcssuraj@gmail.com>
2026-06-08 17:52:46 +0530

    Staring

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int var1 = 30;
+        int var2 = 23;
+        System.out.print(var1+var2);
+        int a = 2;
+        int b = 4;
+        System.out.print(a+b);
+    }
+}
