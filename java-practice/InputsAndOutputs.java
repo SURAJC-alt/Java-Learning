72ad0f082d05694a4db1ff4b60d48b8c4fc66368
SURAJC <csurajcssuraj@gmail.com>
2026-06-10 20:13:51 +0530

    Inputs and Outputs

---
+++ java-practice/InputsAndOutputs.java
@@ -0,0 +1,11 @@
+//java inputs and outputs:
+import java.util.Scanner;
+public class InputsAndOutputs {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.print(a);
+    }
+
+}
+
