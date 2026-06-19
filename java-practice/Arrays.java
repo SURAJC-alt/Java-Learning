3c3a4016cce6295da69bf3bd0c35b7cd66ea0db6
SURAJC-alt <csurajcssuraj@gmail.com>
2026-06-19 20:44:08 +0530

    Arrays.java

---
+++ java-practice/Arrays.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Arrays {
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //primitive Array syntax:
+        int[] arr; //Declaration 
+        arr = new int[5]; //initialization, now the array looks like this: {0,0,0,0,0,0}
+        // or :
+        int[] number = {1,2,3,4,5,6,7};
+        
+        //non primitive array(ex: String):
+        String[] arr; //Declaration 
+        arr = new String[5]; //initialization, now the array looks like this: {null,null,null,null,null}
+        arr[0] = "suraj";
+        System.out.println(arr[0]+" "+arr[1]);// output: suarj null
+
+        //Array Indexing:
+        int[] arr = {1,2,43,43,6,9};
+                  //[0,1,2,3,4,5,6] : index of the array
+        
+        //inputing the array elements via for loop:
+        int[] arr = new int[5];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        //outputing the array elemets:
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        // or Enhanced for loop of array:
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+        // or Arrays to String method:
+        System.out.println(Arrays.toString(arr));
+    }
+}
